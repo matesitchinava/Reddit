994349ded888ec32736312fe295de56af67977a6
Idris Paragulgov <idris-kartoev@mail.ru>
2024-03-25 16:49:37 +0400

    reddit

---
+++ Entities/Community.cs
@@ -0,0 +1,14 @@
+﻿namespace Reddit.Entities
+{
+    public class Community
+    {
+        public int Id { get; set; }
+        public int OwnerId { get; set; }
+        public User Owner { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public ICollection<Post> Posts { get; set; }
+        public ICollection<User> UserSubscribers { get; set; }
+    }
+}
